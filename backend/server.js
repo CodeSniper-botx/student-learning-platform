4f2032dffa5f42a76e7ddde88e047ed78978703a
CodeSniper-botx <shubhiajju.24@gmail.com>
2026-04-05 02:34:33 +0530

    fix cors issue

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,14 @@ const app = express();
 connectDB();
 
 // 🔥 Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://codesniper-botx.github.io"
+    ],
+    credentials: true
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
